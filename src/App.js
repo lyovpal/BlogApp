84cd6e278cc3e314f8f59748ef82eec49af1cce1
Levon Palikyan <l.palikyan@softconstruct.local>
2020-09-04 17:10:14 +0400

    feat: getting users data and rednring

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from "react";
-import "./App.css";
+import "./styles/styles.css";
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: null,
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users/ ")
+      .then((resp) => resp.json())
+      .then((data) => this.setState({ users: data }));
+  }
+  getId (e) {
+
+  }
+  render() {
+    // console.log(this.state.users);
+    return (
+      <div className="wrapper">
+        <div>
+          <ul>
+            {this.state.users &&
+              this.state.users.map((item) => <li onClick={this.getId} key={item.id}>{item.name}</li>)}
+          </ul>
+        </div>
+        <div>
+          post block
+        </div>
+        <div>
+          comments block
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
